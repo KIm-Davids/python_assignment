984dcdaeaef776f6817471a03f724715800b48d1
KimP0SSIB1E <165786987+KimP0SSIB1E@users.noreply.github.com>
2024-04-05 23:36:13 +0100

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,5 @@
+total = 1;
+counter = 1;
+for number in range(1, 11):
+	if(number % 2 == 0):
+		print(number)
---
+++ Task3.py
@@ -0,0 +1,5 @@
+counter = 1;
+for number in range(10):
+	if(number % 2 == 1):
+		print(number)
+	
