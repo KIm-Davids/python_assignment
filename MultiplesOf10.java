4f829eefd3079bd59859c2bd5953a0a672f6aa01
KimP0SSIB1E <165786987+KimP0SSIB1E@users.noreply.github.com>
2024-04-10 08:11:12 +0100

    MultiplesOf10.java

--- MultiplesOf10.java
+++ MultiplesOf10.java
@@ -1,10 +1,7 @@
 public class MultiplesOf10 {
 	public static void main(String[] args) {
 		
-	int multiples = 1;
 	int count = 1;
-	int number_to_times = 10;	
-	int number_to_check = 0;
 	int sum = 1;
 	for(count = 10; count <= 20000; count++) {
 		if(count % 10 == 0){
