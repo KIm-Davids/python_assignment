f7ec1bd0287b3fb61d3e4f7e8674af152595cb5f
KimP0SSIB1E <165786987+KimP0SSIB1E@users.noreply.github.com>
2024-04-25 07:45:33 +0100

    Sikiru Assignment

--- check_number.py
+++ check_number.py
@@ -1,3 +1,33 @@
-check_number = input("Enter a word")
+from python_functions import length
+from python_functions import two_length
+from python_functions import replace
+from python_functions import find_long_word
+from python_functions import odd_index
 
-print(length_of_a_string(check_number))
+
+def test_length_python_functions():
+        assert length("semicolon") == 9
+        
+def test_two_length_python_functions():        
+        assert two_length('semicolon') == 'seon'
+        assert two_length('on') == 'onon'
+        assert two_length('o') == "" 
+        
+def test_replace_python_functions():
+        assert replace('abc') == 'abcing'
+        assert replace('string') == 'stringly'
+        assert replace('on') == 'on'
+        
+def test_find_long_word_python_functions():
+
+	word_inputted = ["welcome", "out", "weather", "mobile", "Breakfast", "journey"]
+
+	long_word, long_length = find_long_word(word_inputted) 
+   
+	assert long_word == 'Breakfast' and long_length == 9
+
+def test_odd_index_python_functions():
+
+	assert odd_index('semicolon') == 'eioo'
+
+        
--- python_functions.py
+++ python_functions.py
@@ -1,6 +1,41 @@
-def length_of_a_string(number):
+def length(word_inputted):
 
-	length_of_the_word = (len(number))
-        return length_of_the_word
+        return len(word_inputted)
+        
+        
+def two_length(word_inputted):
 
-	
+        if len(word_inputted) < 2:
+            return ""
+        else:
+            return word_inputted[:2] + word_inputted[-2:]
+            
+def replace(word_inputted):
+    
+        if len(word_inputted) < 3:
+                return word_inputted 
+        elif word_inputted[-3:] == 'ing':
+                return word_inputted + 'ly'
+        else:
+            return word_inputted[-3:] + 'ing'
+            
+def find_long_word(word_inputted):
+
+	long_word = ""
+	long_length = 0
+
+	for word in word_inputted:
+		if len(word) > long_length:
+			long_word = word
+			long_length = len(word)
+	return long_word, long_length
+
+def odd_index(word_inputted):
+	word_counted = ""
+	for word in range(len(word_inputted)):
+		if word % 2 == 1:
+			word_counted += word_inputted[word]
+	return word_counted
+            
+    
+      
