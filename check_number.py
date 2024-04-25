d2e0b6900b6b4f6c01e8ee9ebc377a5abc151d62
KimP0SSIB1E <165786987+KimP0SSIB1E@users.noreply.github.com>
2024-04-25 05:49:54 +0100

    Sikiru Test Assignment

---
+++ check_number.py
@@ -0,0 +1,3 @@
+check_number = input("Enter a word")
+
+print(length_of_a_string(check_number))
---
+++ python_functions.py
@@ -0,0 +1,6 @@
+def length_of_a_string(number):
+
+	length_of_the_word = (len(number))
+        return length_of_the_word
+
+	
