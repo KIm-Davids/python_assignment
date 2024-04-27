867e976d16ddf2aeb66a54eb92907c780ed7a740
KimP0SSIB1E <165786987+KimP0SSIB1E@users.noreply.github.com>
2024-04-27 09:19:38 +0100

    Updated function assignment

--- check_number.py
+++ check_number.py
@@ -3,7 +3,11 @@ from python_functions import two_length
 from python_functions import replace
 from python_functions import find_long_word
 from python_functions import odd_index
-
+from python_functions import two_input
+from python_functions import maximum_number
+from python_functions import minimum_number
+from python_functions import doubled_numbers
+from python_functions import summed_doubled_numbers
 
 def test_length_python_functions():
         assert length("semicolon") == 9
@@ -29,5 +33,28 @@ def test_find_long_word_python_functions():
 def test_odd_index_python_functions():
 
 	assert odd_index('semicolon') == 'eioo'
+
+def test_two_input_python_functions():
+	assert two_input('hello',3) == 'hellohellohello'
+	assert two_input('hi',4.5) == 'hi'
+
+def test_minimum_number_python_fuctions():
+	number_inputted = [8,4,9,2,5,7,3]
+	assert minimum_number(number_inputted) == 2
+
+def test_maximum_number_python_fuctions():
+	number_inputted = [8,4,9,2,5,7,3]
+	assert maximum_number(number_inputted) == 9
+
+def test_doubled_numbers_python_functions():
+	numbers_inputted = [2,3,4,5,7]
+	assert doubled_numbers(numbers_inputted) == [4,9,16,25,49]
+
+def test_summed_doubled_numbers_python_functions():
+
+	numbers_inputted = [2,3,4,5,7]
+	assert summed_doubled_numbers(numbers_inputted) == 103 
+
+
 
         
--- python_functions.py
+++ python_functions.py
@@ -32,10 +32,76 @@ def find_long_word(word_inputted):
 
 def odd_index(word_inputted):
 	word_counted = ""
-	for word in range(len(word_inputted)):
-		if word % 2 == 1:
-			word_counted += word_inputted[word]
+	for counter in range(len(word_inputted)):
+		if counter % 2 == 1:
+			word_counted += word_inputted[counter]
 	return word_counted
+
+
+
+
+def minimum_number(number_inputted):
+
+	
+	minimum = number_inputted[0]
+
+	for number in number_inputted:
+		if number < minimum:  
+			minimum = number
+	return minimum
+
+
+def maximum_number(number_inputted):
+
+
+	maximum = number_inputted[0]
+
+	for number in number_inputted:
+
+		if number > maximum:
+			maximum = number
+
+	return maximum
+
+
+def two_input(string, num):
+
+	if type(string) and type(num) in [string, int]:
+		return string * num
+	else:
+		return string 
+
+
+def doubled_numbers(numbers_inputted):
+	
+	numbers_inputted = [2,3,4,5,7]
+	new_list = []
+
+	for numbers in numbers_inputted:
+		new_list = [numbers ** 2 for numbers in numbers_inputted]
+	return new_list
+
+def summed_doubled_numbers(numbers_inputted):
+
+	numbers_inputted = [2,3,4,5,7]
+
+	new_list = []
+	sum = 0
+	for numbers in numbers_inputted:
+		new_list = [numbers ** 2 for numbers in numbers_inputted]
+		
+	for number in new_list:
+		sum += number
+
+	
+	return sum
+
+
+
+
+
+
+
             
     
       
