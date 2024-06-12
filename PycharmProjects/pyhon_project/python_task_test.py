a6fa6f5834271ba021d25b8af7bc8f521b139475
KimP0SSIB1E <165786987+KimP0SSIB1E@users.noreply.github.com>
2024-06-12 07:06:04 +0100

    python assignment

---
+++ PycharmProjects/pyhon_project/python_task_test.py
@@ -0,0 +1,47 @@
+import unittest
+from python_task import *
+
+
+class MyTestCase(unittest.TestCase):
+    def test_length(self):
+        self.assertEqual(7, length_method([1, 2, 3, 4, 5, 6, 7]))
+
+    def test_to_get_the_sum_of_even_index(self):
+        self.assertEqual(12, sum_of_even_indxes([1, 2, 3, 4, 5, 6, 7]))
+
+    def test_to_get_the_sum_of_odd_index(self):
+        self.assertEqual(16, sum_of_odd_indexes([1, 2, 3, 4, 5, 6, 7]))
+
+    def test_to_get_the_product_of_the_index(self):
+        self.assertEqual(28, multiply_third_position([1, 2, 3, 4, 5, 6, 7]))
+
+    def test_to_get_the_average_of_the_index(self):
+        self.assertEqual(4, calculate_average([1, 2, 3, 4, 5, 6, 7]))
+
+    def test_to_get_the_largest_of_the_index(self):
+        self.assertEqual(10, largest_number_in_the_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+
+    def test_to_get_the_smallest_of_the_index(self):
+        self.assertEqual(1, smallest_number_in_the_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+
+    def test_to_get_the_length_of_a_string(self):
+        self.assertEqual("sonos", length_of_a_string(["semicolon", "finish", "hundred", "sonos"]))
+
+    def test_to_get_an_inclusive_list(self):
+        self.assertEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], inclusive_list())
+
+    def test_to_check_for_duplicates_number(self):
+        self.assertEqual(
+            {1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15},
+            duplicate_number_list())
+
+    def test_to_remove_for_duplicates_number(self):
+        self.assertEqual({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}, eliminate_duplicate_values())
+
+    def test_to_add_to_the_third_index(self):
+        self.assertEqual(12, add_the_third_position([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+    def test_add_the_third_position(self):
+        self.assertEqual(add_the_first_middle_and_last_index())
+if __name__ == '__main__':
+    unittest.main()
